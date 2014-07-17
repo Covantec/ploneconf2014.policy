c84add45d5097ca5cc3756c14201efa563bd926a
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2014-07-17 14:23:25 -0430

    First commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+Plone policy for Ploneconf2014 demo, for Mastering Plone in Cantv.com.ve
+
+This product may contain traces of nuts.
---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='ploneconf2014.policy',
+      version=version,
+      description="Plone policy for Ploneconf2014 demo",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='ploneconf policy demo training Cantv.com.ve',
+      author='Leonardo J. Caballero G.',
+      author_email='leonardocaballero@gmail.com',
+      url='https://github.com/Covantec/ploneconf2014.policy',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ploneconf2014'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'collective.plonetruegallery==3.4.4',
+          'collective.geo.bundle==2.1',
+          'Products.PloneFormGen==1.7.14',
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
